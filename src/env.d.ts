f50a88be88e08d2a563e552970737fe8c2362bf9
Aziz Mevlana Alim <azizalim41@gmail.com>
2026-04-28 20:53:00 +0300

    bug fix

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,8 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+interface Window {
+  openDavetiyeModal: (src: string) => void;
+  davetiye25Src: string;
+  davetiye26Src: string;
+}
